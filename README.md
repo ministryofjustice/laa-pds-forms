34cbe725b4182336668891a49ec23a004ca1fab3
Asrar hussain <asra.hussain@digital.justice.gov.uk>
2018-02-08 12:52:48 +0000

    First commit for laa pds forms.  Currently included is spring boot project setup to run over a postgres databse.  I have also included setup for running liquibase locally.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # laa-pds-forms
 laa pds forms
+
+
+== Setting up postgres locally
+
+from the terminal  window type psql
+ 
+CREATE ROLE pds WITH LOGIN PASSWORD ‘password’;
+\q
+psql postgres -U pds
+CREATE DATABASE laa_pds_forms;
+GRANT ALL PRIVILEGES ON DATABASE laa_pds_forms TO pds;
+
+== running  liquibase
+
---
+++ src/main/java/com/digital/laa/LaaPdsFormsApplication.java
@@ -0,0 +1,12 @@
+package com.digital.laa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaaPdsFormsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaaPdsFormsApplication.class, args);
+	}
+}
